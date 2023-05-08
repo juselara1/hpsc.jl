31e65e75213a5381ba6b9e9268e28f3c7f1ced0b
Juan Lara <julara@unal.edu.co>
2023-05-08 18:55:01 -0500

    Completion of numerical operators

--- src/hpml.jl/src/4_numerical_operators.jl
+++ src/hpml.jl/src/4_numerical_operators.jl
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -14,6 +15,245 @@
 # # Numerical Operators
 # ---
 #
-# There are different operators that work between two `Julia` numbers, this includes arithmetic, comparison, logical and bitwise.
+# There are different types of operators that work between two `Julia` numbers. These include arithmetic, comparison, logical, and bitwise operators.
 
+# %% [markdown]
+# ## Arithmetic
+# ---
+#
+# You can perform classical arithmetic operations between two numbers in `Julia`. Whenever it's possible, the types are automatically cast in a convenient way. For instance, we can sum two integers with the `+` operator:
+#
+# %%
+x = Int8(2) + Int16(3);
+print(x)
+print(typeof(x))
 
+# %% [markdown]
+# As you can see, the result has the higher numerical precision between the operands. Likewise, if anything is operated with a `Float` value, the results will be cast into floats:
+
+# %%
+x = Int32(2) + Float16(3);
+print(x)
+print(typeof(x))
+
+# %% [markdown]
+# Let's see the arithmetic operations.
+#
+# - **Addition**: this is performed with the `+` operator:
+
+# %%
+x = 3. + 4;
+print(x)
+
+# %% [markdown]
+# - **Substraction**: this is performed with the `-` operator:
+
+# %%
+x = 3. - 1.2;
+print(x)
+
+# %% [markdown]
+# - **Multiplication**: this is performed with the `*` operator:
+
+# %%
+x = 3. * complex(2, 3);
+print(x)
+
+# %% [markdown]
+# - **Division**: this is performed with the `/` operator:
+
+# %%
+x = 3.2 / 1.2;
+print(x)
+
+# %% [markdown]
+# - **Reverse division**: this is performed with the `\` operator:
+
+# %%
+x = 1.2 \ 3.2;
+print(x)
+
+# %% [markdown]
+# - **Exponentiation**: this is performed with the `^` operator:
+
+# %%
+x = 2 ^ 3;
+print(x)
+
+# %% [markdown]
+# - **Modulo**: this is performed with the `%` operator:
+
+# %%
+x = 5 % 2;
+print(x)
+
+# %% [markdown]
+# ## Comparison
+# ---
+#
+# We can use comparison operators to validate conditions over numerical values and returns a boolean value of the operation.
+#
+# - **Equality**: this is performed with the `==` operator:
+
+# %%
+x = Int8(2) == Int16(2);
+print(x)
+
+# %% [markdown]
+# - **Difference**: this is performed with the `!=` operator or the `≠` symbol:
+
+# %%
+x = Float16(0.0) != Int8(0);
+print(x)
+
+# %% [markdown]
+# - **Inequalities**: we have four kind of inequalities. Less than uses the `<` operator, greater than uses the `>` operator, less or equal than uses the `<=` or the `≤` operator, and greater or equal uses the `>=` or the `≥` operator:
+
+# %%
+x = Int8(2) < 3f0;
+print(x)
+
+# %%
+x = Int16(32) >= UInt8(3);
+print(x)
+
+# %%
+x = Float16(4) <= 3.5;
+print(x)
+
+# %% [markdown]
+# ## Logical
+# ---
+#
+# These operators work over boolean values and implement the three basic boolean algebra operations:
+#
+# - **And**: it is implemented with the `&&` operator:
+
+# %%
+x = false && false;
+print(x)
+
+# %%
+x = false && true;
+print(x)
+
+# %%
+x = true && false;
+print(x)
+
+# %%
+x = true && true;
+print(x)
+
+# %% [markdown]
+# - **Or**: it is implemented with the `||` operator:
+
+# %%
+x = false || false;
+print(x)
+
+# %%
+x = false || true;
+print(x)
+
+# %%
+x = true || false;
+print(x)
+
+# %%
+x = true || true;
+print(x)
+
+# %% [markdown]
+# - **Not**: it is implemented with the `!` operator:
+
+# %%
+x = ! true;
+print(x)
+
+# %%
+x = ! false;
+print(x)
+
+# %% [markdown]
+# ## Bitwise
+# ---
+#
+# Bitwise operations are manipulate the binary representation of integers in `Julia`. The following operators are available:
+#
+# - **Bitwise and**: this is implemented with the `&` operator. Let's see an example of the and operation between two bytes of data:
+
+# %%
+x = 0b10001000 & 0b10000000;
+print(x)
+
+# %% [markdown]
+# This operation is equivalent too:
+
+# %%
+x = UInt8(136) & UInt8(128);
+print(x)
+
+# %% [markdown]
+# Also with hex literals:
+
+# %%
+x = 0x88 & 0x80;
+print(x)
+
+# %% [markdown]
+# - **Bitwise or**: this is implemented with the `|` operator:
+
+# %%
+x = 0b00001111 | 0b11110000;
+print(x)
+
+# %% [markdown]
+# - **Bitwise not**: this is implemented with the `~` operator:
+
+# %%
+x = ~ 0b11110000;
+print(x)
+
+# %% [markdown]
+# - **Left shift**: this is implemented with the `<<` operator. It's useful for low-level integer multiplication:
+
+# %%
+x = 15 << 1; # 15 * 2
+print(x)
+
+# %%
+x = 15 << 2; # 15 * 4
+print(x)
+
+# %%
+x = 15 << 3; # 15 * 8
+print(x)
+
+# %% [markdown]
+# - **Right shift**: this is implemented with the `>>` operator. It's useful for low-level integer division:
+
+# %%
+x = 64 >> 1; # 64 / 2
+print(x)
+
+# %%
+x = 64 >> 2; # 64 / 4
+print(x)
+
+# %%
+x = 64 >> 3; # 64 / 8
+print(x)
+
+# %% [markdown]
+# ## Updating Operators
+# ---
+#
+# These operators allow you to reassign the result of a variable after operating with its value.
+#
+# Any arithmetic or bitwise operator can be transformed into an updating operator by placing the assignment operator `=` after the operator. For instance, the operator `+=` takes what was previously assigned to a variable, adds a value and reassigns the result:
+
+# %%
+x = 1.5;
+x += 2;
+print(x)
