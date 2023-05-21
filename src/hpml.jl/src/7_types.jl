d5e0504a6f236d74a0376936e0e4306653c7c3a2
Juan Lara <julara@unal.edu.co>
2023-05-21 09:53:33 -0500

    Completion of types notebook

--- src/hpml.jl/src/7_types.jl
+++ src/hpml.jl/src/7_types.jl
@@ -124,24 +124,24 @@ try
 catch err
     print(err)
 end
- 
+
 # %% [markdown]
 # However, we can define mutable types using the `mutable` keyword as follows:
- 
+
 # %% [code]
 mutable struct MutCartesianPoint64
     x::Float64
     y::Float64
 end
- 
+
 # %% [markdown]
 # Let's define a new point:
- 
+
 # %% [code]
 p = MutCartesianPoint64(1, 2)
 p.x = 3
 print(p)
- 
+
 # %% [markdown]
 # ## Type Generation
 # ---
@@ -152,14 +152,82 @@ print(p)
 # ### Sub-Typing
 # ---
 #
+# Sub-typing allows to define new types under an existing type, this is achived through the `<:` operator.
 #
+# This is useful if we want to define code that may work over different data types. For instance, we can define the `AbstractShape` type:
+
+# %% [code]
+abstract type AbstractShape end;
+
+# %% [markdown]
+# Now, we can define different shapes:
+
+# %% [code]
+struct Circle64 <: AbstractShape
+    radius::Float64
+end
+
+struct Rectangle64 <: AbstractShape
+    width::Float64
+    height::Float64
+end
+
+# %% [markdown]
+# We can create a circle and a rectagle as follows:
+
+# %% [code]
+c = Circle64(1.0);
+r = Rectangle64(1.0, 2.0);
+
+# %% [markdown]
+# We can validate if a variable has some type using `isa`: 
+
+# %% [code]
+print(isa(c, Circle64))
+print(isa(c, AbstractShape))
+
+# %% [code]
+print(isa(r, Circle64))
+print(isa(r, AbstractShape))
 
 # %% [markdown]
 # ### Unions
 # ---
 #
+# Union types allow creating a new type from the sum or union of other types, this is useful when we want to operate in the same way over different types that don't have a common ancestor.
+#
+# These types can be defined using `Union` as follows:
+
+# %% [code]
+Num64 = Union{Int64, Float64}
+
+# %% [markdown]
+# In this case `Num64` may be an `Int64` or a `Float64`, we can validate this:
+
+# %% [code]
+print(isa(1, Num64))
+print(isa(1.0, Num64))
+
+# %% [code]
+print(isa(0x01, Num64))
+print(isa(1f0, Num64))
 
 # %% [markdown]
 # ### Parametric
 # ---
 #
+# Parametric types allow to define new types using a type as parameter, this prevents the multiple definition of a single composite type with different numerical precisions or sub-types.
+#
+# Let's see an example with a more general `Circle` type that doesn't enforce a numerical precision:
+
+# %% [code]
+struct Circle{T}
+    radius::T
+end
+
+# %% [markdown]
+# We can define a circle with an `UInt8` radius:
+
+# %% [code]
+c2 = Circle{UInt8}(2);
+print(c2)
